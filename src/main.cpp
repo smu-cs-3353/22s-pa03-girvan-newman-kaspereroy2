ffbcbff39f21b265b1732f7b3ac64a59edbe724b
Super1717 <78092391+Super1717@users.noreply.github.com>
2022-04-12 23:55:26 -0500

    Added program arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
-
-#include <iostream>
-#include <fstream>
+#include <string>
 #include "GraphHandler.h"
 
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
 
 
-int main() {
+int main(int argc, char* argv[]) {
     GraphHandler handler;
-    Graph g = handler.readGraph("../data/football.graphml");
-    handler.girvanNewman(g,12);
-    handler.writeGraphml(g,"../results/football11.graphml");
+    std::string graphmlFile = argv[1];
+    Graph g = handler.readGraph(graphmlFile);
+    int desiredGroups = atoi(argv[2]);
+    handler.girvanNewman(g,desiredGroups);
+    std::string outputFile = argv[3];
+    handler.writeGraphml(g,outputFile);
 
     return 0;
 }
+
